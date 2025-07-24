3f0d437fbd759e655ab66fb9f88e8ca1da7cbac3
Maykmora <maykmora12@gmail.com>
2025-07-24 17:17:00 -0600

    Commit 1

---
+++ ejercicio_01.py
@@ -0,0 +1,12 @@
+def menu():
+    print("\n--MENÚ--")
+    print("1.Encontrar suma, promedio, multiplos de 3 de n numeros")
+    print("2.Calcular el área de un rectangulo")
+    print("3.Verificar si un numero es primo o no")
+    print("4.Calcular el promedio de n calificaciones")
+    print("5.Calcular el numero mayor y el menor de n números ")
+    print("6.Calculadora")
+    print("7.Salir")
+
+while True:
+    menu()
